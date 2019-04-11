29609488bd36882d5d8e7c1a7ecccde2397889bb
Anthony Poon <ypoon196@gmail.com>
2019-04-11 14:50:47 +0800

    Fix last commit not addoing API

--- src/share/API.js
+++ src/share/API.js
@@ -6,52 +6,13 @@ const API = {
     root: ENV_API,
     login: () => URI(ENV_API + "/auth/login").normalize().toString(),
     register: () => URI(ENV_API + "/auth/register").normalize().toString() ,
-    admin: {
-        users: () => URI(ENV_API + "/admin/users").normalize().toString(),
-        testCenters: ({ testCenterId } = {}) => {
-            const template = new URITemplate(ENV_API + "/admin/test-centers/{testCenterId}");
-            return URI(template.expand({
-                testCenterId: testCenterId
-            })).normalize().toString();
-        },
-        exams: ({ examId } = {}) => {
-            const template = new URITemplate(ENV_API + "/admin/exams/{examId}");
-            return URI(template.expand({
-                examId: examId
-            })).normalize().toString();
-        },
-        holidays: ({ testCenterId }) => {
-            const template = new URITemplate(ENV_API + "/admin/test-centers/{testCenterId}/holiday");
-            return URI(template.expand({
-                testCenterId: testCenterId
-            })).normalize().toString();
-        }
-    },
     member: {
-        exams: ({ examId } = {}) => {
-            const template = new URITemplate(ENV_API + "/member/exams/{examId}");
+        example: ({ exampleId } = {}) => {
+            const template = new URITemplate(ENV_API + "/member/{exampleId}");
             return URI(template.expand({
-                examId: examId
+                exampleId: exampleId
             })).normalize().toString();
         },
-        testCenters: ({ testCenterId } = {}) => {
-            const template = new URITemplate(ENV_API + "/member/test-centers/{testCenterId}");
-            return URI(template.expand({
-                testCenterId: testCenterId
-            })).normalize().toString();
-        },
-        holidays: ({ testCenterId }) => {
-            const template = new URITemplate(ENV_API + "/member/test-centers/{testCenterId}/holidays");
-            return URI(template.expand({
-                testCenterId: testCenterId
-            })).normalize().toString();
-        },
-        timeSlots: ({ testCenterId }) => {
-            const template = new URITemplate(ENV_API + "/member/test-centers/{testCenterId}/time-slots");
-            return URI(template.expand({
-                testCenterId: testCenterId
-            })).normalize().toString();
-        }
     }
 };
 
